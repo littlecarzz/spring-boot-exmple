83d16e3ea326314d9fb1497a0ea740d4e0f45b13
littlecar <xc_chejj@163.com>
2019-08-19 17:24:16 +0800

    20190819

---
+++ spring-boot-thymeleaf/spring-boot-thymeleaf-layout/src/main/java/com/cjj/ThymeleafLayoutApplication.java
@@ -0,0 +1,17 @@
+package com.cjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：
+ *
+ * @author littlecar
+ * @date 2019/8/19 17:23
+ */
+@SpringBootApplication
+public class ThymeleafLayoutApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafLayoutApplication.class, args);
+    }
+}
